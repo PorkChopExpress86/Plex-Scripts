9dc57d39afefb9797dbe9b997aa42b4f6f538a7f
PorkChopExpress86 <29725019+PorkChopExpress86@users.noreply.github.com>
2023-10-17 13:26:17 -0500

    inital commit

---
+++ install_plex.sh
@@ -0,0 +1,19 @@
+sudo apt install curl ufw -y
+
+curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
+echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
+sudo apt update
+sudo apt unstall plexmediaserver -y
+
+# Copy the UFW txt over to the area
+sudo cp ./plexmediaserver /etc/ufw/applications.d/plexmediaserver
+
+# Apply UFW rules for plexmediaserver
+sudo ufw app update plexmediaserver
+sudo ufw allow plexmediaserver-all
+sudo ufw status verbose
+
+
+# Create folder for media
+sudo mkdir -p /opt/plexmedia/{movies,series}
+sudo chown -R plex: /opt/plexmedia
