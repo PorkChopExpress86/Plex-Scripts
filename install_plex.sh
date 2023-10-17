8ec85219bb79861b8987bef47cdc00c5116fd86f
PorkChopExpress86 <29725019+PorkChopExpress86@users.noreply.github.com>
2023-10-17 13:46:18 -0500

    Fixed install typo

--- install_plex.sh
+++ install_plex.sh
@@ -3,7 +3,7 @@ sudo apt install curl ufw -y
 curl https://downloads.plex.tv/plex-keys/PlexSign.key | sudo apt-key add -
 echo deb https://downloads.plex.tv/repo/deb public main | sudo tee /etc/apt/sources.list.d/plexmediaserver.list
 sudo apt update
-sudo apt unstall plexmediaserver -y
+sudo apt install plexmediaserver -y
 
 # Copy the UFW txt over to the area
 sudo cp ./plexmediaserver /etc/ufw/applications.d/plexmediaserver
